a2934e3024cebc643654486b7d36cbf6964908f2
Sam Curley <scurley@huskers.unl.edu>
2018-02-01 12:22:15 -0600

    commit

--- c_version/kaprekarTest.c
+++ c_version/kaprekarTest.c
@@ -30,9 +30,10 @@ int main(int argc, char **argv) {
     if(contains(kaprekarNumbers, numKaprekarNumbers, i) != isKaprekar(i)) {
       numWrong++;
       if(contains(kaprekarNumbers, numKaprekarNumbers, i)) {
-	fprintf(stderr, "i = %d: is a known Kaprekar number, but the method returned false\n", i);
-      } else {
-	fprintf(stderr, "i = %d: is known NOT to be a Kaprekar number, but the method returned true\n", i);
+	printf("i = %d: is a known Kaprekar number, but the method returned false\n", i);
+      }
+      else {
+	printf("i = %d: is known NOT to be a Kaprekar number, but the method returned true\n", i);
       }
     } else {
       numCorrect++;
--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -1,4 +1,4 @@
-#include<math.h>
+#include <math.h>
 
 #include "kaprekarUtils.h"
 
@@ -25,27 +25,26 @@
 int isKaprekar(int n) {
 
   if(n < 1) {
-    return false;
+    return 0;
   }
 
   int i;
   long square = n * (long) n;
   int numDigits = (int) log10(n) + 1;
-  long modulus = 0;
+  long modulus = 1;
   long first, second;
 
   //for each possible "split" of the square...
-  for(i=1; i<=numberOfDigits; i++) {
+  for(i=1; i<=numDigits; i++) {
     //increase the modulus by a factor of 10
-    modulous *= 10;
+    modulus *= 10;
 
     //split the square into two parts
     first = square / modulus;
     second = square % modulus;
 
     //test if the split makes a Kaprekar number
-    if(second > 0 &&
-       first + second == n) {
+    if(second > 0 && first + second == n) {
       return 1;
     }
   }
